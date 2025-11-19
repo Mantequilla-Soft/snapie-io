6a7d0fe0853a88b74056f1a631a3a4863baafa23
Eddie Espino <e.espinodeeke@gmail.com>
2025-11-19 06:11:24 -0600

    Add login button to mobile footer navigation

--- components/layout/FooterNavigation.tsx
+++ components/layout/FooterNavigation.tsx
@@ -1,12 +1,17 @@
-import { useAioha } from '@aioha/react-ui';
-import { Box, Button, HStack, Icon, Tooltip } from '@chakra-ui/react';
+import { useAioha, AiohaModal } from '@aioha/react-ui';
+import { Box, Button, HStack, Icon, Tooltip, useColorMode } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
-import { FiBell, FiBook, FiCreditCard, FiHome, FiUser } from 'react-icons/fi';
+import { FiBell, FiBook, FiCreditCard, FiHome, FiUser, FiLogIn } from 'react-icons/fi';
+import { KeyTypes } from '@aioha/aioha';
+import { useState } from 'react';
+import '@aioha/react-ui/dist/build.css';
 
 export default function FooterNavigation() {
 
     const { user } = useAioha();
     const router = useRouter();
+    const { colorMode } = useColorMode();
+    const [modalDisplayed, setModalDisplayed] = useState(false);
     const handleNavigation = (path: string) => {
         if (router) {
             router.push(path);
@@ -36,16 +41,6 @@ export default function FooterNavigation() {
                     />
                 </Tooltip>
 
-                <Tooltip label="Wallet" aria-label="Wallet tooltip">
-                    <Button
-                        onClick={() => handleNavigation("/@" + user + '/wallet')}
-                        variant="ghost"
-                        leftIcon={<Icon as={FiCreditCard} boxSize={5} />}
-                        _hover={{ bg: "muted" }}
-                        isDisabled={!user}
-                    />
-                </Tooltip>
-
                 <Tooltip label="Blog" aria-label="Blog tooltip">
                     <Button
                         onClick={() => handleNavigation("/blog")}
@@ -54,24 +49,54 @@ export default function FooterNavigation() {
                     />
                 </Tooltip>
 
-                <Tooltip label="Notifications" aria-label="Notifications tooltip">
-                    <Button
-                        onClick={() => handleNavigation("/@" + user + "/notifications")}
-                        variant="ghost"
-                        leftIcon={<Icon as={FiBell} boxSize={4} />}
-                        isDisabled={!user}
-                    />
-                </Tooltip>
+                {user ? (
+                    <>
+                        <Tooltip label="Notifications" aria-label="Notifications tooltip">
+                            <Button
+                                onClick={() => handleNavigation("/@" + user + "/notifications")}
+                                variant="ghost"
+                                leftIcon={<Icon as={FiBell} boxSize={4} />}
+                            />
+                        </Tooltip>
 
-                <Tooltip label="Profile" aria-label="Profile tooltip">
-                    <Button
-                        onClick={() => handleNavigation("/@" + user)}
-                        variant="ghost"
-                        leftIcon={<Icon as={FiUser} boxSize={4} />}
-                        isDisabled={!user}
-                    />
-                </Tooltip>
+                        <Tooltip label="Wallet" aria-label="Wallet tooltip">
+                            <Button
+                                onClick={() => handleNavigation("/@" + user + '/wallet')}
+                                variant="ghost"
+                                leftIcon={<Icon as={FiCreditCard} boxSize={4} />}
+                            />
+                        </Tooltip>
+
+                        <Tooltip label="Profile" aria-label="Profile tooltip">
+                            <Button
+                                onClick={() => handleNavigation("/@" + user)}
+                                variant="ghost"
+                                leftIcon={<Icon as={FiUser} boxSize={4} />}
+                            />
+                        </Tooltip>
+                    </>
+                ) : (
+                    <Tooltip label="Login" aria-label="Login tooltip">
+                        <Button
+                            onClick={() => setModalDisplayed(true)}
+                            variant="ghost"
+                            leftIcon={<Icon as={FiLogIn} boxSize={4} />}
+                        />
+                    </Tooltip>
+                )}
             </HStack>
+            <div className={colorMode}>
+                <AiohaModal
+                    displayed={modalDisplayed}
+                    loginOptions={{
+                        msg: 'Login',
+                        keyType: KeyTypes.Posting,
+                        loginTitle: 'Login',
+                    }}
+                    onLogin={console.log}
+                    onClose={() => setModalDisplayed(false)}
+                />
+            </div>
         </Box>
     );
 }
